441fc00666b2319556418f4e4d1efb849a489c3d
Quantum Mechanics <writewale@netscape.net>
2013-07-26 19:49:10 -0400

    initial prototype working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 WeightTracker::Application.routes.draw do
+  devise_for :users
+  root 'static_pages#home'
+  #match '/profile', to: 'static_pages#profile', via: [:get,:post]
+  resources :weights, only: [:index, :new, :create, :edit]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
